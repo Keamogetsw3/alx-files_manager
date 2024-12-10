ae77444f5766144f81525488df0111e17137b00e
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2024-12-10 15:14:03 +0200

    Creat a constructor method for the RedisClient class, used to initialize an instance of the class

--- utils/redis.js
+++ utils/redis.js
@@ -1,8 +1,27 @@
 import { promisify } from 'util';
 import { createClient } from 'redis';
-/**
- * module to interact with a Redis database
- */
 
+
+/**
+ * Define a RedisClient class that interact with database
+ */
 class RedisClient {
+  /**
+  * Initializes the RedisClient instance.
+  * - Creates a client to Redis using the `createClient` function.
+  * - Tracks the connection state through the `isClientConnected` property.
+  * - Attaches event listeners to handle connection and error events:
+  *   - On `error`: Logs the error and sets `isClientConnected` to `false`.
+  *   - On `connect`: Logs successful connection and sets `isClientConnected` to `true`.
+  */
+  constructor() {
+    this.client = createClient();
+    this.isClientConnected = true;
+    this.client.on('error', (err) => {
+      console.error('Redis client failed to connect:', err.message || err.toString());
+      this.isClientConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
+    });
 }
