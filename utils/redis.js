4877b5cb2444f6e61339dd0bb592a7ae10eeffd1
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2024-12-11 20:11:13 +0200

    Update redis.js: RedisClient should have:  the constructor that creates a client to Redis

--- utils/redis.js
+++ utils/redis.js
@@ -1,35 +1,43 @@
-/* eslint-disable import/no-named-as-default */
-import { expect } from 'chai';
-import redisClient from '../../utils/redis';
+/**
+ *  file redis.js that contains the class RedisClient
+ */
+import { promisify } from 'util';
+import { createClient } from 'redis';
 
-describe('+ RedisClient utility', () => {
-  before(function (done) {
-    this.timeout(10000);
-    setTimeout(done, 4000);
-  });
 
-  it('+ Client is alive', () => {
-    expect(redisClient.isAlive()).to.equal(true);
-  });
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.isClientConnected = true;
+    this.client.on('error', (err) => {
+      console.error('Redis client failed to connect:', err.message || err.toString());
+      this.isClientConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
+    });
+  }
 
-  it('+ Setting and getting a value', async function () {
-    await redisClient.set('test_key', 345, 10);
-    expect(await redisClient.get('test_key')).to.equal('345');
-  });
+  isAlive() {
+    return this.isClientConnected;
+  }
 
-  it('+ Setting and getting an expired value', async function () {
-    await redisClient.set('test_key', 356, 1);
-    setTimeout(async () => {
-      expect(await redisClient.get('test_key')).to.not.equal('356');
-    }, 2000);
-  });
+ 
+  async get(key) {
+    return promisify(this.client.GET).bind(this.client)(key);
+  }
 
-  it('+ Setting and getting a deleted value', async function () {
-    await redisClient.set('test_key', 345, 10);
-    await redisClient.del('test_key');
-    setTimeout(async () => {
-      console.log('del: test_key ->', await redisClient.get('test_key'));
-      expect(await redisClient.get('test_key')).to.be.null;
-    }, 2000);
-  });
-});
+ 
+  async set(key, value, duration) {
+    await promisify(this.client.SETEX)
+      .bind(this.client)(key, duration, value);
+  }
+
+ 
+  async del(key) {
+    await promisify(this.client.DEL).bind(this.client)(key);
+  }
+}
+
+export const redisClient = new RedisClient();
+export default redisClient;
