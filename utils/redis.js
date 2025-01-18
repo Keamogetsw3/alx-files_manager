c54a3f379dbf5adc7cd1714dd009d7e50efbffe4
Keamogetsw3 <keamogetswemaswanganyi@gmail.com>
2025-01-18 12:59:30 -0800

    n

--- utils/redis.js
+++ utils/redis.js
@@ -4,7 +4,6 @@
 import { promisify } from 'util';
 import { createClient } from 'redis';
 
-
 class RedisClient {
   constructor() {
     this.client = createClient();
@@ -22,18 +21,15 @@ class RedisClient {
     return this.isClientConnected;
   }
 
- 
   async get(key) {
     return promisify(this.client.GET).bind(this.client)(key);
   }
 
- 
   async set(key, value, duration) {
     await promisify(this.client.SETEX)
       .bind(this.client)(key, duration, value);
   }
 
- 
   async del(key) {
     await promisify(this.client.DEL).bind(this.client)(key);
   }
